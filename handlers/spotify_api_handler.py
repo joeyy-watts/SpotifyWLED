34dffad2aaa6342a1fc727a79cb66b2a4c57c347
joeyy_watts <joeyms40@gmail.com>
2023-10-15 21:26:17 +0700

    add AudioFeatures class to spotify api handler

--- handlers/spotify_api_handler.py
+++ handlers/spotify_api_handler.py
@@ -25,6 +25,45 @@ class TrackObject:
             self.cover_url = None
             self.is_playing = False
 
+class AudioFeatures:
+    def __init__(self, danceability: float, energy: float, key: int, loudness: float, mode: int,
+                 speechiness: float, acousticness: float, instrumentalness: float,
+                 liveness: float, valence: float, tempo: float):
+        self.danceability = danceability
+        self.energy = energy
+        self.key = key
+        self.loudness = loudness
+        self.mode = mode
+        self.speechiness = speechiness
+        self.acousticness = acousticness
+        self.instrumentalness = instrumentalness
+        self.liveness = liveness
+        self.valence = valence
+        self.tempo = tempo
+
+    @classmethod
+    def empty(cls):
+        return AudioFeatures(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    @classmethod
+    def is_none(cls):
+        return all(value == 0.0 if isinstance(value, float) else value == 0 for value in cls.__dict__.values())
+
+    @classmethod
+    def from_dict(cls, audio_features_dict: dict):
+        return cls(
+            audio_features_dict.get('danceability', 0.0),
+            audio_features_dict.get('energy', 0.0),
+            audio_features_dict.get('key', 0),
+            audio_features_dict.get('loudness', 0.0),
+            audio_features_dict.get('mode', 0),
+            audio_features_dict.get('speechiness', 0.0),
+            audio_features_dict.get('acousticness', 0.0),
+            audio_features_dict.get('instrumentalness', 0.0),
+            audio_features_dict.get('liveness', 0.0),
+            audio_features_dict.get('valence', 0.0),
+            audio_features_dict.get('tempo', 0.0))
+
 class SpotifyAPIHandler:
     def __init__(self, client_id: str, client_secret: str):
         self.spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(
@@ -34,6 +73,7 @@ class SpotifyAPIHandler:
             scope="user-read-currently-playing,user-read-playback-state"))
 
         self.current_track: TrackObject = TrackObject(None)
+        self.audio_features: AudioFeatures = AudioFeatures.empty()
         self.last_api_call_time = time.time()
 
     def __handle_api_interval(self):
@@ -56,7 +96,11 @@ class SpotifyAPIHandler:
         if self.current_track is None:
             self.update_current_track()
 
-        return self.spotify.audio_features(self.current_track.track_id)
+        if self.audio_features.is_none():
+            json = self.spotify.audio_features(self.current_track.track_id)
+            self.audio_features = AudioFeatures.from_dict(json[0])
+
+        return self.audio_features
 
     def get_current_track_cover(self):
         return self.current_track.cover_url
