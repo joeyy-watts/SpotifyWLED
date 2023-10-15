b5fec63cad474d0648d14a3dff323dbbd76e1fdd
joeyy_watts <joeyms40@gmail.com>
2023-10-15 17:51:49 +0700

    fix extraneous API call in __artnet_loop

--- handlers/main_http_handler.py
+++ handlers/main_http_handler.py
@@ -76,10 +76,12 @@ class AioMainHTTPHandler():
 
         for ArtNet, polling is handled by the internal WLEDArtNet async stop loops.
         """
-        while(True):
-            self.api_handler.update_current_track()
-            current_track = self.api_handler.get_current_track()
+        # update for initial loop only
+        # subsequent updates are handled in WLEDArtNet handler
+        self.api_handler.update_current_track()
 
+        while(True):
+            current_track = self.api_handler.get_current_track()
             # nothing is playing, wait and continue loop
             if current_track is None:
                 sleep(POLLING_SECONDS)
