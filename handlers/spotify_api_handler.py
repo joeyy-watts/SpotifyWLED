6fc71fec2a309de80b22b6fdb97562c59e2d0987
joeyy_watts <joeyms40@gmail.com>
2023-10-15 21:09:05 +0700

    fix get_audio_features

--- handlers/spotify_api_handler.py
+++ handlers/spotify_api_handler.py
@@ -55,7 +55,8 @@ class SpotifyAPIHandler:
     def get_audio_features(self):
         if self.current_track is None:
             self.update_current_track()
-        return self.spotify.audio_features(self.current_track)
+
+        return self.spotify.audio_features(self.current_track.track_id)
 
     def get_current_track_cover(self):
         return self.current_track.cover_url
