7286f1654eb0cede3b62bb0aabd3e46f015bc7ae
joeyy_watts <joeyms40@gmail.com>
2023-10-15 22:37:29 +0700

    update effects

--- utils/effects/effects.py
+++ utils/effects/effects.py
@@ -19,7 +19,7 @@ class PlaybackEffects(WaveformEffects):
         :return: list of brightness factors
         """
         main_pulse = self.sinus_raw(a=0.5, p=2, v=0.5)
-        breathe_pulse = self.trunc_sinus(a=0.3, p=0.0001, v=0.7)
+        breathe_pulse = self.trunc_sinus_raw(a=0.3, p=0.0001, v=0.7)
         crest_idx = floor(len(main_pulse) / 4)
 
         # splice the main pulse with breathing at the crest
@@ -31,7 +31,7 @@ class PlaybackEffects(WaveformEffects):
         :param period: period of the sin wave
         :return: list of brightness factors
         """
-        return self.sinus(a=0.3, p=period, v=0.5)
+        return self.sinus_raw(a=0.3, p=period, v=0.5)
 
     def bpm_play(self, t_audio_features: AudioFeatures):
         """
@@ -42,13 +42,17 @@ class PlaybackEffects(WaveformEffects):
 
         """
         IDEAS for using Audio Features:
+            - waveform influenced by:
+                - high energy/danceability, more sudden changes -> sawtooth/trunc sin
+                - low energy/danceability, more gradual changes -> sinusoidal
+                
             - amplitude influenced by:
                 - loudness
                 - energy
                 - danceability
                 - bassiness (not sure which metric to use for this)
                 
-            - bpm_factor influenced by:
+            - bpm_div_factor influenced by:
                 - energy
                 - danceability
                 
@@ -59,7 +63,5 @@ class PlaybackEffects(WaveformEffects):
         even better effects can be achieve with Audio Analysis: https://developer.spotify.com/documentation/web-api/reference/get-audio-analysis
         but that's some time off..
         """
-        # this makes animation smoother than using raw bpm
-        bpm_factor = 2
 
-        return self.sinus_bpm(bpm=t_audio_features.tempo/bpm_factor, a=0.3, v=0.6)
+        return self.trunc_sinuc_bpm(bpm=t_audio_features.tempo, a=0.3, v=0.6)
