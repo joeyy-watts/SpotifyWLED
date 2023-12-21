f0f4720cddfe2bacb43bbb587737aa9bba0e4773
joeyy_watts <joeyms40@gmail.com>
2023-12-22 00:39:36 +0700

    adjust default effects resolution

--- confs/global_confs.py
+++ confs/global_confs.py
@@ -3,4 +3,6 @@ Various global configurations.
 """
 
 # how many factors to calculate for effects
-EFFECTS_RESOLUTION = 360
+# I suspect high values cause time shift due to the time to complete setting each frame
+# It is suggested to keep it reasonably low
+EFFECTS_RESOLUTION = 24
