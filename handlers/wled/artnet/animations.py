6e8bde483cb08d776f8f8c46cc687abd008fa84b
joeyy_watts <joeyms40@gmail.com>
2023-12-23 18:22:08 +0700

    add AnimateCover base class; refactor animations into classes

--- handlers/wled/artnet/animations.py
+++ handlers/wled/artnet/animations.py
@@ -1,46 +1,139 @@
 import asyncio
+import time
+from typing import final
 
-from confs.global_confs import TARGET_FPS
+from confs.global_confs import TARGET_FPS, IDLE_TIMEOUT
 from handlers.artnet.artnet_handler import ArtNetHandler
-from handlers.wled.WLEDState import WLEDState
+from handlers.spotify_api_handler import SpotifyAPIHandler, TrackObject
 from utils.async_utils import ManagedCoroutineFunction
 from utils.effects.base_effects import EffectData
+from utils.effects.effects import PlaybackEffects
 from utils.effects.effects_utils import is_black
+from utils.image_utils import get_cover
 
 
 class AnimateCover(ManagedCoroutineFunction):
-    def __init__(self, handler: ArtNetHandler, image, effect_data: EffectData, player_state: WLEDState):
+    def __init__(self,
+                 width: int,
+                 height: int,
+                 handler: ArtNetHandler,
+                 api_handler: SpotifyAPIHandler,
+                 track: TrackObject
+                 ):
         """
-        Class for animating a given image with the given EffectData
+        Base class for all animations
+
         :param handler: ArtNetHandler
         :param image: image to animate
         :param effect_data: the EffectData object with calculated effects
         :param player_state: the player state associated with the animation
         """
-        super().__init__()
         self.handler: ArtNetHandler = handler
-        self.image = image
-        self.effect_data = effect_data
-        self.player_state = player_state
+        self.api_handler: SpotifyAPIHandler = api_handler
+        self.image = get_cover(self.api_handler.get_current_track_cover(), (width, height))
+        self.effect_data = self._get_effect_data()
+        self.displaying_tid = track.track_id
 
+        super().__init__()
+
+    @final
     async def _main_function(self):
         """
-        plays the play animation
-        :param image: image to animate
-        :param effect_data: the EffectData object for a given calculated effect
+        Main function that plays animation
         """
-        for i in self.effect_data.factors:
-            # TODO: for brighter pixels, apply factor at 1.0 multiplier
-            # for darker pixels, apply factor scaled to absolute brightness
+        while not self.stop_event.is_set():
+            for i in self.effect_data.factors:
+                # TODO: for brighter pixels, apply factor at 1.0 multiplier
+                # for darker pixels, apply factor scaled to absolute brightness
 
-            # TODO: refactor into separate function
-            await self.handler.set_pixels([[int(r * i), int(g * i), int(b * i)]
-                                           if not is_black((r, g, b)) else
-                                           [int(r), int(g), int(b)]
-                                           for r, g, b in self.image])
+                # TODO: refactor into separate function
+                await self.handler.set_pixels([[int(r * i), int(g * i), int(b * i)]
+                                               if not is_black((r, g, b)) else
+                                               [int(r), int(g), int(b)]
+                                               for r, g, b in self.image])
 
-            # have to await according to target FPS
-            await asyncio.sleep(self.effect_data.period / TARGET_FPS)
+                # have to await according to target FPS
+                await asyncio.sleep(self.effect_data.period / TARGET_FPS)
 
     async def _stop_function(self, stop_event):
-        pass
+        raise NotImplementedError
+
+    def _get_effect_data(self) -> EffectData:
+        raise NotImplementedError
+
+
+class PlayCover(AnimateCover):
+    def __init__(self,
+                 width: int,
+                 height: int,
+                 handler: ArtNetHandler,
+                 api_handler: SpotifyAPIHandler,
+                 track: TrackObject
+                 ):
+        self.width = width
+        self.height = height
+        self.track = track
+
+        super().__init__(width, height, handler, api_handler, track)
+
+    def _get_effect_data(self) -> EffectData:
+        return PlaybackEffects(self.width, self.height).bpm_play(self.api_handler.get_audio_features())
+
+    async def _stop_function(self, stop_event):
+        current_track = self.api_handler.update_current_track()
+
+        if not current_track.is_playing \
+                or current_track.track_id != self.track.track_id:
+            stop_event.set()
+
+
+class PauseCover(AnimateCover):
+    def __init__(self,
+                 width: int,
+                 height: int,
+                 handler: ArtNetHandler,
+                 api_handler: SpotifyAPIHandler,
+                 track: TrackObject
+                 ):
+        self.width = width
+        self.height = height
+        self.track = track
+
+        super().__init__(width, height, handler, api_handler, track)
+
+    def _get_effect_data(self) -> EffectData:
+        return PlaybackEffects(self.width, self.height).pause()
+
+    async def _stop_function(self, stop_event):
+        current_track = self.api_handler.update_current_track()
+
+        if current_track.is_playing \
+                or current_track.track_id != self.track.track_id:
+            stop_event.set()
+
+
+class IdleCover(AnimateCover):
+    def __init__(self,
+                 width: int,
+                 height: int,
+                 handler: ArtNetHandler,
+                 api_handler: SpotifyAPIHandler,
+                 track: TrackObject
+                 ):
+        self.width = width
+        self.height = height
+        self.track = track
+        self.idle_start_time = time.time()
+
+        super().__init__(width, height, handler, api_handler, track)
+
+    def _get_effect_data(self) -> EffectData:
+        return PlaybackEffects(self.width, self.height).pause()
+
+    async def _stop_function(self, stop_event):
+        current_track = self.api_handler.update_current_track()
+
+        if time.time() - self.idle_start_time > IDLE_TIMEOUT \
+                or current_track.track_id is not None:
+            stop_event.set()
+
--- handlers/wled/wled_artnet.py
+++ handlers/wled/wled_artnet.py
@@ -1,24 +1,10 @@
-import asyncio
-from enum import Enum
-
-from confs.global_confs import TARGET_FPS
 from handlers.artnet.artnet_handler import ArtNetHandler, WLEDArtNetMode
-from handlers.spotify_api_handler import SpotifyAPIHandler, AudioFeatures
+from handlers.spotify_api_handler import SpotifyAPIHandler
+from handlers.wled.artnet.animations import PlayCover, PauseCover, IdleCover
 from handlers.wled.wled_handler import BaseWLEDHandler
-from utils.effects.base_effects import EffectData
-from utils.effects.effects import PlaybackEffects
-from utils.effects.effects_utils import is_black
-from utils.image_utils import get_cover
 
-# TODO: find better image for idle animation
-IDLE_IMAGE = 'https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs'
 
 class WLEDArtNet(BaseWLEDHandler):
-
-    class WLEDState(Enum):
-        PLAYING = 0
-        PAUSED = 1
-
     def __init__(self, address: str, width: int, height: int, spotify_handler: SpotifyAPIHandler):
         super().__init__(address, width, height)
         self.api_handler = spotify_handler
@@ -31,112 +17,30 @@ class WLEDArtNet(BaseWLEDHandler):
             # update current track
             current_track = self.api_handler.update_current_track()
 
-            # TODO: add idle animation with Spotify logo: https://storage.googleapis.com/pr-newsroom-wp/1/2021/02/Spotify_Icon_RGB_Green.png
+            # idle animation, if no track is playing
             if current_track.track_id is None:
-                print("No track playing, displaying idle image")
-                await self.__pause_cover(get_cover(IDLE_IMAGE, self.size))
+                await IdleCover(
+                    self.size[0],
+                    self.size[1],
+                    self.handler,
+                    self.api_handler,
+                    current_track
+                ).run()
 
             # run appropriate animation (play/pause)
             if current_track.is_playing:
-                self.animating_track = current_track
-                await self.__play_cover(get_cover(self.api_handler.get_current_track_cover(), self.size), self.api_handler.get_audio_features())
+                await PlayCover(
+                    self.size[0],
+                    self.size[1],
+                    self.handler,
+                    self.api_handler,
+                    current_track
+                ).run()
             else:
-                self.animating_track = current_track
-                await self.__pause_cover(get_cover(self.api_handler.get_current_track_cover(), self.size))
-
-    async def __play_cover(self, image, t_audio_features: AudioFeatures):
-        """
-        applies the playing animation to the given cover
-        :param image: current track
-        :param t_audio_features: a dict containing audio features of the track
-        :return: an asyncio task
-        """
-        factors = PlaybackEffects(self.size[0], self.size[1]).bpm_play(t_audio_features)
-        return await asyncio.create_task(self.__animate_cover_task(image, factors, WLEDArtNet.WLEDState.PLAYING))
-
-    async def __pause_cover(self, image):
-        """
-        applies the paused animation to the given cover
-        :param image: current track
-        :return: an asyncio task
-        """
-        factors = PlaybackEffects(self.size[0], self.size[1]).pause()
-        return await asyncio.create_task(self.__animate_cover_task(image, factors, WLEDArtNet.WLEDState.PAUSED))
-
-    async def __stop_loop(self, stop_event, state: WLEDState):
-        self.current_tid = self.api_handler.current_track.track_id
-        while not await self.__should_stop(state):
-            # update current track every stop lop
-            self.api_handler.update_current_track()
-
-            # TODO: change this to use global POLLING_SECONDS
-            await asyncio.sleep(3)
-
-        stop_event.set()
-
-    async def __should_stop(self, state: WLEDState):
-        current_track = self.api_handler.get_current_track()
-        if state == WLEDArtNet.WLEDState.PLAYING:
-            # if current state is playing, the loop should break if:
-            #   - track is now paused
-            #   - track has changed
-            if not current_track.is_playing \
-                    or current_track.track_id != self.current_tid:
-                return True
-
-        if state == WLEDArtNet.WLEDState.PAUSED:
-            # if current state is paused, the loop should break if:
-            #   - track is now playing
-            #   - track has changed, even if paused
-            #   - TODO: if WLED is powered off
-            if current_track.is_playing \
-                    or current_track.track_id != self.current_tid:
-                return True
-
-        return False
-
-    async def __animate_cover_task(self, image, effect_data: EffectData, state):
-        """
-        an asyncio task to animate cover in the background
-        :param image: image to animate
-        :param effect_data: the EffectData object for a given calculated effect
-        :return: asyncio coroutine
-        """
-        # setup stop event
-        stop_event = asyncio.Event()
-        asyncio.create_task(self.__stop_loop(stop_event, state))
-
-        while True:
-            # check for stop event
-            if stop_event.is_set():
-                # TODO:
-                # if stop event is set for PLAYING state, it means:
-                #   - song has changed -> break into outer loop to update cover
-                #   - song is now paused -> change state ONLY
-                # if stop event is set for PAUSED state:
-                #   - song has changed -> break into outer loop to update cover
-                #   - song is now playing -> change state ONLY
-                # so, if still same song, no need to break into outer loop
-                # just need to change play state
-
-                stop_event.clear()
-                # second break; break animation loop itself
-                break
-            else:
-                for i in effect_data.factors:
-                    # TODO: for brighter pixels, apply factor at 1.0 multiplier
-                    # for darker pixels, apply factor scaled to absolute brightness
-
-
-                    # first break; break the animation cycle
-                    if stop_event.is_set():
-                        break
-                    # TODO: refactor into separate function
-                    await self.handler.set_pixels([[int(r*i), int(g*i), int(b*i)]
-                                                   if not is_black((r,g,b)) else
-                                                   [int(r), int(g), int(b)]
-                                                    for r, g, b in image])
-
-                    # have to await according to target FPS
-                    # TODO: replace 1 second with closest appropriate value to avoid wave clipping
-                    await asyncio.sleep(effect_data.period / TARGET_FPS)
+                await PauseCover(
+                    self.size[0],
+                    self.size[1],
+                    self.handler,
+                    self.api_handler,
+                    current_track
+                ).run()
