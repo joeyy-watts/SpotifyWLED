2ab140e15b5ac34fbf4a7d2e657e0ed3e740b483
joeyy_watts <joeyms40@gmail.com>
2023-12-24 22:32:09 +0700

    raise default TARGET_FPS to 44

--- confs/global_confs.py
+++ confs/global_confs.py
@@ -5,7 +5,7 @@ Various global configurations.
 # Target FPS for animations
 # I suspect high values cause time shift due to the time to complete setting each frame
 # It is suggested to keep it reasonably low
-TARGET_FPS = 24
+TARGET_FPS = 44
 
 POLLING_SECONDS = 2
 
