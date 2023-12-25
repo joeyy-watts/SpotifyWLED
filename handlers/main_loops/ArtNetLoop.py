4d33ba440d28bc1e2cce27f8179e26cc2df16820
joeyy_watts <joeyms40@gmail.com>
2023-12-26 00:26:33 +0700

    implement .exit() in ArtNetLoop

--- handlers/main_loops/ArtNetLoop.py
+++ handlers/main_loops/ArtNetLoop.py
@@ -3,12 +3,21 @@ from utils.async_utils import ManagedCoroutineFunction
 
 
 class ArtNetLoop(ManagedCoroutineFunction):
-    def __init__(self, handler: WLEDArtNet):
-        self.handler: WLEDArtNet = handler
+    """
+    Loop for animating WLED devices via ArtNet
+
+    .exit() should be used instead of .stop() to ensure that the ArtNet refresh is stopped
+    """
+    def __init__(self, wled_handler: WLEDArtNet):
+        self.wled_handler: WLEDArtNet = wled_handler
         super().__init__()
 
+    def exit(self):
+        self.wled_handler.artnet_handler.node.stop_refresh()
+        self.stop()
+
     async def _main_function(self):
-        await self.handler.animate()
+        await self.wled_handler.animate()
 
     async def _stop_function(self):
         pass
